8e607d1faa29d5233e7f35cb0708445de112336d
João Brázio <jbrazio@gmail.com>
2023-05-06 16:58:40 +0100

    Fix msbuild buildroot preparation

--- build.bat
+++ build.bat
@@ -5,6 +5,8 @@
 	del /q "%~dp0.release\*.*"
 ) 2>NUL
 
+dotnet build %~dp0\ExampleModule /t:DownloadToolsTask --nologo
+
 for /r %~dp0 %%D in (*.csproj) do (
 	dotnet build "%%D" --configuration Release --nologo
 	copy "%%~pDbin\Release\*.dll" "%~dp0.release"
