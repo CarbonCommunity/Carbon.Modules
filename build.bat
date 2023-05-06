013392227f3e385f957e9c73f86c16ce8f332e1f
João Brázio <jbrazio@gmail.com>
2023-05-06 16:38:39 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,12 +1,11 @@
 @echo off
 
-mkdir "%~dp0.release" 2>NUL
-del /q "%~dp0.release\*.*" 2>NUL
+(
+	mkdir "%~dp0.release"
+	del /q "%~dp0.release\*.*"
+) 2>NUL
 
-for /d %%D in (%~dp0*) do (
-	echo "%%D" | findstr /i /L ".release" >NUL
-	if errorlevel 1 (
-		dotnet build "%%D" --configuration Release --nologo
-		copy "%%D\bin\Release\*.exe" "%~dp0.release"
-	)
+for /r %~dp0 %%D in (*.csproj) do (
+	dotnet build "%%D" --configuration Release --nologo
+	copy "%%~pDbin\Release\*.dll" "%~dp0.release"
 )
